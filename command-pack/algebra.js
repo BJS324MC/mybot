791904f7168ddf91817bc2d2f380755cb5719810
??? <bjs332244@gmail.com>
2021-10-19 11:07:38 +0800

    algebra

---
+++ command-pack/algebra.js
@@ -0,0 +1,35 @@
+const {simplify, derivative, evaluate} = require('mathjs');
+module.exports = {
+    data: {
+        name: "algebra",
+        description: "A list of algebra commands for working with algebra.",
+        options: [{
+            "name": "simpify",
+            "description": "Simplify an equation.",
+            "type": 1,
+            "options":[
+                {
+                    name:"equation",
+                    description:"An equation as input. Example: 2x - (2 ^ 3) * x",
+                    type:3,
+                    required:true
+                }
+            ]
+        }]
+    },
+    async execute(interaction) {
+        const eq = interaction.options.data[0].value;
+
+        await interaction.reply({embeds: [{
+            color:0x0099dd,
+            title:"Equation Simplifier",
+            description:`To simplify the equation ${eq}`,
+            fields:[
+                {
+                    name:"1. Simplify.",
+                    value:simplify(eq)
+                }
+            ]
+        }]});
+    },
+};
